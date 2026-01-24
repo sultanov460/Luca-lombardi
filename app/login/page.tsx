4fc14aeb51175cbac922677847216571be9c9606
Rafiq Sultanov <sultanovrafiq71@gmail.com>
2026-01-24 07:14:19 +0400

    sign and sign-up zod

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 import { Container } from "@/components/Container";
+import { signSchema } from "@/schemas/sign";
 import { LoginFormData } from "@/types/login";
 import Link from "next/link";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { IoEye, IoEyeOff } from "react-icons/io5";
+import z from "zod";
+
+interface ErrorsState {
+  email: string | null;
+  password: string | null;
+}
 
 export default function Login() {
   const [formData, setFormData] = useState<LoginFormData>({
@@ -11,15 +18,49 @@ export default function Login() {
     password: "",
   });
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
+  const [errors, setErrors] = useState<ErrorsState>({
+    email: null,
+    password: null,
+  });
 
   const [show, setShow] = useState(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: null });
+  }
+
+  function handleLogin(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const result = signSchema.safeParse(formData);
+
+    if (!result.success) {
+      const flattened = z.flattenError(result.error);
+
+      const fieldErrors = flattened.fieldErrors;
+
+      setErrors({
+        email: fieldErrors.email?.[0] || null,
+        password: fieldErrors.password?.[0] || null,
+      });
+
+      return;
+    }
+
+    const validatedData = result.data;
+
+    setErrorMessage(null);
+  }
+
   return (
     <div className="pt-20 pb-30">
       <Container>
-        <form className="gird grid-cols-1 gap-5 shadow-2xl rounded-3xl p-8 w-full sm:w-150 mx-auto">
+        <form
+          onSubmit={handleLogin}
+          className="gird grid-cols-1 gap-5 shadow-2xl rounded-3xl p-8 w-full sm:w-150 mx-auto"
+        >
           <h1 className="text-4xl tracking-[2px] text-center mb-5">Sign In</h1>
           <div className="flex flex-col gap-5">
             <div className="flex flex-col relative">
@@ -31,6 +72,9 @@ export default function Login() {
                 value={formData.email}
                 name="email"
               />
+              {errors.email && (
+                <span className="text-red-500 text-sm">{errors.email}</span>
+              )}
             </div>
             <div className="flex flex-col relative">
               <input
@@ -40,6 +84,9 @@ export default function Login() {
                 onChange={handleChange}
                 name="password"
               />
+              {errors.password && (
+                <span className="text-red-500 text-sm">{errors.password}</span>
+              )}
               <button
                 className="absolute top-3 right-5 cursor-pointer"
                 onClick={() => setShow(!show)}
