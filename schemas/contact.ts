b5d980cc60a9a8b70d0276dd2ea6370830017c3d
Rafiq Sultanov <sultanovrafiq71@gmail.com>
2026-07-04 13:07:27 +0400

    contaqct error change

--- schemas/contact.ts
+++ schemas/contact.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const contactSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 characters"),
   email: z.email("Invalid email"),
-  message: z.string().min(1, "Gijdillax yazda nese"),
+  message: z.string().min(1, "Please enter your message"),
   phone: z.string().optional(),
 });
 
